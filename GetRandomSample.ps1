cb0554d0e466e98a781c93462ecffc0894670407
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-03-22 17:29:58 +0200

    Add script GetRandomSample.ps1

---
+++ GetRandomSample.ps1
@@ -0,0 +1,68 @@
+param (
+    [Parameter(mandatory = $true)] $inputFilePath,
+    $sampleLineCount = 1,
+    $outputFilePath,
+    [switch] $preserveFirstLine
+)
+
+Function Add-SuffixToFileName($filePath, $suffix) {
+    [IO.Path]::ChangeExtension($filePath, "").TrimEnd(".") + `
+    $suffix + `
+    [IO.Path]::GetExtension($filePath)
+}
+
+Function Get-TotalLineCount($filePath) {
+    $reader = [IO.File]::OpenText($inputFilePath)
+    try {
+        $result = 0;
+        while (!$reader.EndOfStream) {
+            $reader.ReadLine() | Out-Null
+            $result++;
+            if ($result % 100000 -eq 0) { Write-Host "Counting line $result" }
+        }
+    }
+
+    finally {
+        $reader.Dispose();
+    }
+    return $result
+}
+
+$outputFilePath ??= Add-SuffixToFileName $inputFilePath "_sample"
+
+Write-Host "Counting number of lines..."
+$totalLineCount = Get-TotalLineCount $inputFilePath
+Write-Host "Total number of lines: $totalLineCount"
+
+If ($totalLineCount -eq 0) {
+    throw "Cannot create sample from empty file"
+}
+Write-Host "Reading the file..."
+$reader = [IO.File]::OpenText($inputFilePath)
+try {
+    $writer = [IO.File]::CreateText($outputFilePath)
+    try {
+        $lineNumber = 0
+        $lineCountToSelect = $sampleLineCount + ($preserveFirstLine ? 1 : 0)
+        $random = New-Object Random
+        while (!$reader.EndOfStream -and $lineCountToSelect -gt 0) {
+            $line = $reader.ReadLine()
+            $lineCountToRead = $totalLineCount - $lineNumber
+            $selectionProbability = [Math]::Min($lineCountToSelect / $lineCountToRead, 1.0)
+            If ($preserveFirstLine -and $lineNumber -eq 0 `
+                    -or $random.NextSingle() -le $selectionProbability) {
+                $writer.WriteLine($line)
+                $lineCountToSelect--
+            }
+            $lineNumber++
+            if ($lineNumber % 100000 -eq 0) { Write-Host "Processing line $lineNumber" }
+        }
+    }
+    finally {
+        $writer.Dispose();
+    }
+}
+finally {
+    $reader.Dispose();
+}
+Write-Host "Samle file with $sampleLineCount lines is successfully created"
--- RepeatFile.ps1
+++ RepeatFile.ps1
@@ -1,14 +1,15 @@
-Param (
+param (
     $inputFile,
     $repeatCount = 2
 )
 
-$outputFileName = (Join-Path (Get-Item $inputFile).DirectoryName (Get-Item $inputFile).Basename) + 
+$outputFileName =
+    (Join-Path (Get-Item $inputFile).DirectoryName (Get-Item $inputFile).Basename) + `
     "_x$repeatCount" + [IO.Path]::GetExtension($inputFile);
 
 if (Test-Path $outputFileName) { Remove-Item $outputFileName }
 
 $content = Get-Content $inputFile
-For ($i=0; $i -lt $repeatCount; $i++) {
+For ($i = 0; $i -lt $repeatCount; $i++) {
     $content | Out-File $outputFileName -Append -Encoding "UTF8"
 }
