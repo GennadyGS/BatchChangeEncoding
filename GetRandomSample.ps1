50e67ea9ab1bd2ad955041f6f48ae762a8ce8e57
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-12-12 09:16:53 +0200

    Optimize sorting in GetRandomSample.ps1

--- GetRandomSample.ps1
+++ GetRandomSample.ps1
@@ -32,6 +32,7 @@ Function Get-RandomSampleFromReader($reader, $sampleLineCount) {
     $result = [PSCustomObject[]]::new($sampleLineCount)
     $lineIndex = 0
     while (!$reader.EndOfStream) {
+        LogProcessedLines $lineIndex
         $line = $reader.ReadLine()
         $randomIndex = $random.Next($lineIndex + 1)
         if ($randomIndex -lt $sampleLineCount) {
@@ -39,18 +40,25 @@ Function Get-RandomSampleFromReader($reader, $sampleLineCount) {
             $result[$replacedIndex] = @{ Index = $lineIndex; Line = $line }
         }
         $lineIndex++
-        if ($lineIndex % 100000 -eq 0) {
-            Write-Host "Processed $lineIndex lines"
-        }
     }
+    Write-Host "Reading file complete: $lineIndex lines"
     $resultLength = [Math]::Min($sampleLineCount, $lineIndex)
-    return $result[0 .. ($resultLength - 1)]
-        | Sort-Object -Property Index
-        | Select-Object -ExpandProperty Line
+    $trimmedResult = $result[0 .. ($resultLength - 1)]
+    $sortedResult = [System.Linq.Enumerable]::OrderBy(
+        $trimmedResult, [Func[object, object]]{ param($x) $x.Index })
+    return [System.Linq.Enumerable]::Select(
+        $sortedResult, [Func[object, object]]{ param($x) $x.Line })
 }
 
+Function LogProcessedLines($lineCount) {
+    if (($lineCount -gt 0) -and ($lineCount % 100000 -eq 0)) {
+        Write-Host "Processed $lineIndex lines"
+    }
+}
+
+Write-Host "Reading input file $inputFilePath..."
 $sampleLines = Get-SampleLinesFromFile $inputFilePath $sampleLineCount $preserveFirstLine
 $outputFilePath ??= Add-SuffixToFileName $inputFilePath "_sample"
 Write-Host "Writing result to output file $outputFilePath..."
 [IO.File]::WriteAllLines($outputFilePath, $sampleLines)
-Write-Host "Sample file with $($sampleLines.Count) lines is successfully created"
+Write-Host "Output file with $($sampleLines.Count) lines is successfully created"
